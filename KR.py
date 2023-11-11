34ab074fc919d9a16c8dc45be57a117f3946d090
ch1ll1xxc <olegebronze@gmail.com>
2023-11-11 16:43:00 +0300

    created simple window

--- KR.py
+++ KR.py
@@ -4,3 +4,22 @@ from PyQt5.QtGui import *
 from PIL import Image
 from PIL.ExifTags import TAGS
 import os
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Test")
+        self.setMinimumHeight(300)
+        self.setMinimumWidth(500)
+        self.setGeometry(300, 300, 500, 300)
+        self.inverseTAGS = {value: key for key, value in TAGS.items()}
+
+
+def window():
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    window()
