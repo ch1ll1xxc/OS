ba5c9f217e846f189b0f7061a974860e6bf5079e
ch1ll1xxc <olegebronze@gmail.com>
2023-11-13 16:19:00 +0300

    added code comments in Russian

--- KR.py
+++ KR.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import *
 from PIL import Image
 from PIL.ExifTags import TAGS
 import os
+# Создаем класс главного окна
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -32,7 +33,7 @@ class MainWindow(QWidget):
         self.btn_delete.adjustSize()
         self.btn_delete.move(168, 236)
 
-
+    # Функция для выбора изображения
     def selectImage(self):
         file_name, _ = QFileDialog.getOpenFileName(self, "Выберите изображение", "", "Изображения (*.jpg *.png)")
         self.file_name = file_name
@@ -40,28 +41,37 @@ class MainWindow(QWidget):
         if not self.file_name:
             QMessageBox.warning(self, "Ошибка", "Необходимо выбрать изображение!")
             return
+        # Отображаем метаданные о выбранном файле
         self.showMetadata()
 
+    # Функция для отображения метаданных о файле
     def showMetadata(self):
         if self.file_name is None:
             return
+        # Открываем изображение с помощью библиотеки Pillow
         image = Image.open(self.file_name)
+        # Получаем метаданные с помощью метода getexif()
         exifdata = image._getexif()
+        # Проверяем, есть ли метаданные в файле
         if not exifdata:
             self.metadata_field.setText("Файл не содержит метаданных")
             return
+        # Создаем строку для отображения метаданных
         metadata_str = "Метаданные файла:\n"
+        # Проходимся по всем метаданным и добавляем их к строке
         for tag_id in exifdata:
             tag = TAGS.get(tag_id, tag_id)
             data = exifdata.get(tag_id)
             if isinstance(data, bytes):
                 data = data.decode()
             metadata_str += f"{tag}: {data}\n"
+        # Отображаем строку в поле для метаданных
         self.metadata_field.setText(metadata_str)
 
 
-
+# Main function
 def window():
+    # Создаем экземпляр класса главного окна и запускаем приложение
     app = QApplication([])
     window = MainWindow()
     window.show()
